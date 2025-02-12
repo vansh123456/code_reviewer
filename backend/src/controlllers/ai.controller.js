066ee32891b3daf9335ecf703bc531026dafb87a
vansh123456 <vanshbhatnagar14dev@gmail.com>
2025-02-12 23:10:17 +0530

    Backend: add intiial Backend Structure includes initial Google Gemini BoilerPlate Code Includes the basic Route Includes the basic Service Includes the Basic API for AI controller

---
+++ backend/src/controlllers/ai.controller.js
@@ -0,0 +1,13 @@
+const aiService = require('../services/ai.service');
+
+module.exports.getResponse = async (req, res) => { 
+    const prompt = req.query.prompt;
+
+    if(!prompt){
+        return res.status(400).json({message: 'Prompt is required'});
+    }   
+
+    const response = await aiService(prompt);
+    res.send(response);
+    
+}
