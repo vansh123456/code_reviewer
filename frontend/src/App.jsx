eba988b08bfe5ac7ac2cf29c8093d7f622a00b4e
vansh123456 <vanshbhatnagar14dev@gmail.com>
2025-02-16 23:29:37 +0530

    frontend: add deployed link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   },[])
 
   async function reviewCode() {
-    const response = await axios.post('http://localhost:3000/ai/get-review',{code})
+    const response = await axios.post('https://code-reviewer-4m5u.onrender.com/ai/get-review',{code})
     setReview(response.data)
   }
 
